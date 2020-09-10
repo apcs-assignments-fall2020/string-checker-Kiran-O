ddba148da49477bdd52ba30a26040b46631506e6
Kiran-O <22kokelly@athenian.org>
2020-09-10 00:50:46 +0000

    All tests passed

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,26 +4,54 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        char a1 = 'a';
+        char b1 = 'b';
+        char c1 = 'c';
+        int zero = 0;
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i)== a1)
+            zero++; 
+            else if (str.charAt(i)== b1)
+            zero++; 
+            else if(str.charAt(i)== c1)
+            zero++; 
+        }
+        return zero;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+        if (str.contains("the")){
+            return true;
+        }
+        else if (str.contains("The")){
+            return true;
+        }
+        else {
+            return false;
+        }
+        
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        int zero = 0;
+        int qwe = str.length() -1;
+
+        while (qwe > zero){
+            if (str.charAt(zero) != str.charAt(qwe))
+            return false;
+            zero++;
+            qwe--;
+        }
+
+        return true;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
+        System.out.println("Please enter a string");
+        String str = scan.nextLine();
     }
 }
